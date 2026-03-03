81f4abc2c4e8072a309648ad50fb974aaf7719d1
Artyom Marchenko <marchenko.a@selectel.com>
2026-03-03 20:37:59 +0300

    refactoring

--- src/main/kotlin/ru/quipy/config/EventSourcingLibConfiguration.kt
+++ src/main/kotlin/ru/quipy/config/EventSourcingLibConfiguration.kt
@@ -1,9 +1,8 @@
 package ru.quipy.config
 
 import jakarta.annotation.PostConstruct
+import java.util.*
 import org.eclipse.jetty.http2.server.HTTP2CServerConnectionFactory
-import org.eclipse.jetty.util.thread.QueuedThreadPool
-import org.eclipse.jetty.server.ServerConnector
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.web.embedded.jetty.JettyServerCustomizer
@@ -14,16 +13,14 @@ import ru.quipy.core.EventSourcingServiceFactory
 import ru.quipy.payments.api.PaymentAggregate
 import ru.quipy.payments.logic.PaymentAggregateState
 import ru.quipy.streams.AggregateEventStreamManager
-import java.util.*
-
 
 /**
- * This files contains some configurations that you might want to have in your project. Some configurations are
- * made in for the sake of demonstration and not required for the library functioning. Usually you can have even
- * more minimalistic config
+ * This files contains some configurations that you might want to have in your project. Some
+ * configurations are made in for the sake of demonstration and not required for the library
+ * functioning. Usually you can have even more minimalistic config
  *
- * Take into consideration that we autoscan files searching for Aggregates, Events and StateTransition functions.
- * Autoscan enabled via [event.sourcing.auto-scan-enabled] property.
+ * Take into consideration that we autoscan files searching for Aggregates, Events and
+ * StateTransition functions. Autoscan enabled via [event.sourcing.auto-scan-enabled] property.
  *
  * But you can always disable it and register all the classes manually like this
  * ```
@@ -42,17 +39,14 @@ class EventSourcingLibConfiguration {
 
     private val logger = LoggerFactory.getLogger(EventSourcingLibConfiguration::class.java)
 
-    @Autowired
-    private lateinit var eventSourcingServiceFactory: EventSourcingServiceFactory
+    @Autowired private lateinit var eventSourcingServiceFactory: EventSourcingServiceFactory
 
-    @Autowired
-    private lateinit var eventStreamManager: AggregateEventStreamManager
+    @Autowired private lateinit var eventStreamManager: AggregateEventStreamManager
 
-    /**
-     * Use this object to creaEventSourcingServicete/update the aggregate
-     */
+    /** Use this object to create/update the aggregate */
     @Bean
-    fun paymentsEsService() = eventSourcingServiceFactory.create<UUID, PaymentAggregate, PaymentAggregateState>()
+    fun paymentsEsService() =
+            eventSourcingServiceFactory.create<UUID, PaymentAggregate, PaymentAggregateState>()
 
     @PostConstruct
     fun init() {
@@ -70,25 +64,13 @@ class EventSourcingLibConfiguration {
 
     @Bean // hack Jetty to tweak the number of possible https2 streams
     fun jettyServerCustomizer(): JettyServletWebServerFactory {
-        val jettyServletWebServerFactory  = JettyServletWebServerFactory()
+        val jettyServletWebServerFactory = JettyServletWebServerFactory()
+        val c = JettyServerCustomizer {
+            (it.connectors[0].getConnectionFactory("h2c") as HTTP2CServerConnectionFactory)
+                    .maxConcurrentStreams = 10_000_000
+        }
 
-        jettyServletWebServerFactory.addServerCustomizers(JettyServerCustomizer { server ->
-            val threadPool = server.threadPool as QueuedThreadPool
-            threadPool.minThreads = 200
-            threadPool.maxThreads = 2000
-            threadPool.idleTimeout = 60000
-            threadPool.isDetailedDump = true
-
-            server.connectors.forEach { connector ->
-                if (connector is ServerConnector) {
-                    connector.connectionFactories.forEach { c ->
-                        if (c is HTTP2CServerConnectionFactory) {
-                            c.maxConcurrentStreams = 200_000
-                        }
-                    }
-                }
-            }
-        })
+        jettyServletWebServerFactory.serverCustomizers.add(c)
         return jettyServletWebServerFactory
     }
 }
